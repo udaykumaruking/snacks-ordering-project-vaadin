aa1164ad20800d346851984ea7fcb6b4cb2c220c
udaykumaruking <udaykumaruking@gmail.com>
2020-06-06 11:31:57 +0530

    Initial commit

---
+++ src/main/java/com/gmail/udaykumar/GreetService.java
@@ -0,0 +1,12 @@
+package com.gmail.udaykumar;
+
+public class GreetService {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+}
---
+++ src/main/java/com/gmail/udaykumar/MainView.java
@@ -0,0 +1,24 @@
+package com.gmail.udaykumar;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a text field for getting the user name and a button
+ * that shows a greeting message in a notification.
+ */
+@Route("")
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+}
